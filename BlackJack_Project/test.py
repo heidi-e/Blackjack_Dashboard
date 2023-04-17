561cf8197d400e926335d990094356698153f54c
heidi-e <100874463+heidi-e@users.noreply.github.com>
2023-04-17 16:37:19 -0400

    set background

--- BlackJack_Project/test.py
+++ BlackJack_Project/test.py
@@ -19,141 +19,71 @@ import dash_bootstrap_components as dbc
 
 deck = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
+new_deck = [{'label': 'A', 'value': 'A'},
+        {'label': '2', 'value': '2'},
+        {'label': '3', 'value': '3'},
+        {'label': '4', 'value': '4'},
+        {'label': '5', 'value': '5'},
+        {'label': '6', 'value': '6'},
+        {'label': '7', 'value': '7'},
+        {'label': '8', 'value': '8'},
+        {'label': '9', 'value': '9'},
+        {'label': '10', 'value': '10'},
+        {'label': 'J', 'value': 'J'},
+        {'label': 'Q', 'value': 'Q'},
+        {'label': 'K', 'value': 'K'}]
 # margin = position on the dashboard (up or down)
-
-
-
-
-
+# margin-left = position left or right
 # create the app
 app = Dash(__name__)
 
 # define the layout
-app.layout = html.Div([
+app.layout = html.Div(
+    style={
+        'background-image': 'url(https://pl.sterlingcdn.com/wp-content/uploads/sites/3/2018/07/blackjack-classic-background.jpg)',
+        'background-repeat': 'no-repeat',
+        'background-position': 'center t',
+        'background-size': '1500px 900px'},
+    children=[
     html.H1(children = "Blackjack Strategy Dashboard", style = {'textAlign': 'center', 'background-color': 'green'}),
-    html.Div(
+    html.Div(id='house',
         children=
         html.Div([
             html.Img(src='https://www.pngall.com/wp-content/uploads/4/Playing-Card-PNG-Clipart.png',
                      style={'width': '10%', 'display':'inline-block', 'float':'left', 'borderRadius': '5px', 'margin': '20px', 'margin-left': '500px'}),
-            html.Img(src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/08_of_spades.svg/800px-08_of_spades.svg.png',
-                     style={'width': '10%', 'display':'inline-block', 'float':'right', 'borderRadius': '5px', 'margin': '20px', 'margin-right': '500px'})
+            html.Img(src='https://opengameart.org/sites/default/files/card%20back%20red.png',
+                     style={'width': '10%', 'display':'inline-block', 'float':'right', 'borderRadius': '5px', 'margin': '20px', 'margin-right': '500px'}),
+            dcc.Dropdown(id = 'house-dropdown', options = new_deck, value = 'A',
+                         style = {'width': '20%','display':'inline-block', 'float':'left', 'margin': '0px', 'margin-left': '-70px'}),
         ]),
-
-        style={
-        'background-image': 'url(https://pl.sterlingcdn.com/wp-content/uploads/sites/3/2018/07/blackjack-classic-background.jpg)',
-        'background-repeat': 'no-repeat',
-        'background-position': 'center t',
-        'background-size': '1500px 900px',
-
-        },
     ),
-    html.Div(
+    html.Div(id='user',
         children=
         html.Div([
             html.Img(src='https://www.pngall.com/wp-content/uploads/4/Playing-Card-PNG-Clipart.png',
                      style={'width': '10%', 'display': 'inline-block', 'float': 'bottom-left', 'borderRadius': '5px',
-                            'margin': '180px', 'margin-left': '-160px'}),
+                            'margin': '140px', 'margin-left': '-380px'}),
             html.Img(src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/08_of_spades.svg/800px-08_of_spades.svg.png',
                      style={'width': '10%', 'display': 'inline-block', 'float': 'bottom-right', 'borderRadius': '5px',
-                            'margin': '180px', 'margin-left': '0px'})
+                            'margin': '140px', 'margin-left': '30px'}),
+            dcc.Dropdown(id='user-dropdown-1', options= new_deck, value='A',
+                         style={'width': '20%', 'display': 'inline-block', 'float': 'left', 'margin': '-70px',
+                                'margin-left': '270px'}),
+            dcc.Dropdown(id='user-dropdown-2', options= new_deck, value='A',
+                         style={'width': '20%', 'display': 'inline-block', 'float': 'left', 'margin': '-70px',
+                                'margin-left': '430px'}),
         ]),
     )
 ])
 
-
-    # Create the image and sunspot count figures, and put them side by side
-
-"""
-html.Img(src='https://soho.nascom.nasa.gov/data/realtime/hmi_igr/1024/latest.jpg', width='30%', height='30%',
-             style={'display': 'inline-block', 'margin-center': '75px', 'margin-bottom': '100px'}),
-    dcc.Dropdown(id='dropdown',
-                 options=[{'label': 'Sunspot activity (monthly)', 'value': 'sunspot_graph'},
-                          {'label': 'Sunspot cycle', 'value': 'cycle_graph'}],
-                 value='sunspot_graph'),
-
-    dcc.Graph(id='sunspots_line',
-              style={'width': '90vh', 'height': '90vh', 'display': 'inline-block', 'margin-left': '200px'}),
-    html.H5(children='Time Range'),
-    dcc.RangeSlider(years.min(), years.max(), step=None, marks=marks, value=[years.min(), years.max()],
-                    id='sunspot_window_slider'),
-    html.H5(children='Smoothing Amount'),
-    dcc.Slider(1, 12, 1, value=5, id='rolling_window_slider'),
-    # create the cycle graph with the necessary sliders
-    dcc.Graph(id='cycle_variability',
-              style={'width': '125vh', 'height': '90vh', 'display': 'block', 'margin-left': 'auto',
-                     'margin-right': 'auto'}),
-    html.H5(children='Cycle Year'),
-    dcc.Slider(5, 15, 1, value=5, id='cycle_slider'),
-    html.H5(children='Cycle Month'),
-    dcc.Slider(0, 12, 1, value=0, id='cycle_slider_month'),
-    # add references at the bottom
-    html.H6(children='Image Source: https://soho.nascom.nasa.gov/data/realtime/realtime-update.html'),
-    html.H6(children='Graph Data Source: https://www.sidc.be'),"""
-
-"""
-# define how the sunspot number graph reacts to the slider inputs
 @app.callback(
-    Output('sunspots_line', 'figure'),
-    Input('sunspot_window_slider', 'value'),
-    Input('dropdown', 'value'),
-    Input('rolling_window_slider', 'value')
+    Output('house', 'children'),
+    Input('house-dropdown', 'value'),
+    Input('user-dropdown-1', 'value'),
+    Input('user-dropdown-2', 'value')
 )
-def update_sunspots(slider_vals, rolling_window):
-    
-    Updates the sunspot graph with the user inputs
-    :param slider_vals: the user-defined time range for the graph
-    :param rolling_window: the user-defined smoothness level for the rolling average
-    :return: the updated figure
-    
-    # compute the rolling average using the input
-    rolling = spots.rolling(window=rolling_window).mean()
-
-    # construct the figure using the original sunspot counts and the rolling average
-    fig = go.Figure(
-        data=[go.Scatter(x=time, y=spots,
-                         mode='lines', name='Monthly'),
-              go.Scatter(x=time, y=rolling, name='Smoothed')])
-
-    # resize the window to the user's input, and update title and labels
-    fig.update_layout(xaxis_range=slider_vals, title='Monthly Mean Total Sunspots Over Time',
-                      xaxis_title='Time (years)',
-                      yaxis_title='Sunspot Count')
-
-    return fig
 
 
-# define how the cycle graph responds to user input
-# EXTRA CREDIT: I added the functionality for the user to choose the month on top of the year for the cycles
-@app.callback(
-    Output('cycle_variability', 'figure'),
-    Input('cycle_slider', 'value'),
-    Input('cycle_slider_month', 'value')
-)
-def update_cycles(slider_val_year, slider_val_month, sunspots=sunspots):
-    
-    Updates the cycle graph to the user inputs
-    :param slider_val_year: the user-defined year to measure as the cycle
-    :param slider_val_month: the user-defined month to measure as the cycle
-    :param sunspots: passed original dataframe
-    :return: the updated figure
-    
-    # compute the modulo of the time period based on the user input
-    cycle_year = (slider_val_year + (slider_val_month / 12))
-    sunspots = sunspots.assign(overlayed_cycle=time % cycle_year)
-
-    # create the figure
-    fig = go.Figure(
-        data=[go.Scatter(x=sunspots['overlayed_cycle'], y=spots, mode='markers')]
-    )
-
-    # update title, labels
-    fig.update_layout(title=f'Sunspot Cycle: {cycle_year}', xaxis_title='Years',
-                      yaxis_title='Sunspot Count')
-
-    return fig
-
-"""
 def main():
     # run the dashboard
     app.run_server(debug=True)
